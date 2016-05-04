b0e89c7ac54e56c035b2fabd2c09d66d25fe754e
gabricric <gabricric94@gmail.com>
2016-05-04 10:09:02 +0200

    v 0.1 Interfaccia grafica completata

--- Lab8_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
+++ Lab8_MetroDeParis/src/it/polito/tdp/metrodeparis/MetroDeParisController.java
@@ -1,5 +1,44 @@
 package it.polito.tdp.metrodeparis;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+
 public class MetroDeParisController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ComboBox<?> boxStart;
+
+    @FXML
+    private ComboBox<?> boxDest;
+
+    @FXML
+    private Button btnDistance;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doDistance(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert boxStart != null : "fx:id=\"boxStart\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert boxDest != null : "fx:id=\"boxDest\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert btnDistance != null : "fx:id=\"btnDistance\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'MetroDeParis.fxml'.";
+
+    }
 }
