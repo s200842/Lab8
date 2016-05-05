cb926b225a798c35bac0c67714dc1c0b71e87d7f
gabricric <gabricric94@gmail.com>
2016-05-05 12:11:10 +0200

    v 3.0 versione finale dell'algoritmo. Manca miglioramento DB

--- Lab8_MetroDeParis/src/it/polito/tdp/metrodeparis/model/MetroDeParisModel.java
+++ Lab8_MetroDeParis/src/it/polito/tdp/metrodeparis/model/MetroDeParisModel.java
@@ -1,12 +1,11 @@
 package it.polito.tdp.metrodeparis.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.jgrapht.GraphPath;
-import org.jgrapht.Graphs;
 import org.jgrapht.alg.DijkstraShortestPath;
-import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.DirectedWeightedMultigraph;
 
 import com.javadocmd.simplelatlng.LatLng;
 import com.javadocmd.simplelatlng.LatLngTool;
@@ -20,13 +19,13 @@ public class MetroDeParisModel {
 	private FermataDAO fdao;
 	private LineaDAO ldao;
 	private List<Fermata> fermate;
-	private DefaultDirectedWeightedGraph<Fermata, DefaultWeightedEdge> graph;
+	private DirectedWeightedMultigraph<Fermata, DefaultWeightedEdge> graph;
 	
 	public MetroDeParisModel(){
 		fdao = new FermataDAO();
 		fermate = fdao.getFermate();
 		ldao = new LineaDAO();
-		graph = new DefaultDirectedWeightedGraph<Fermata, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		graph = new DirectedWeightedMultigraph<Fermata, DefaultWeightedEdge>(DefaultWeightedEdge.class);
 	}
 	
 	public List<Fermata> getFermate(){
@@ -50,12 +49,17 @@ public class MetroDeParisModel {
 			}
 		}
 		System.out.println(graph.vertexSet());
-		//Collego le stazioni con lo stesso id (cambio linea)
+		//Collego le stazioni con lo stesso id (cambio linea) SE NON ESISTE GIA' un collegamento
 		for(Fermata f : graph.vertexSet()){
 			for(Fermata f1 : graph.vertexSet()){
-				if(f.getIdFermata() == f1.getIdFermata()){
-					graph.addEdge(f, f1);
-					graph.setEdgeWeight(graph.getEdge(f, f1), f1.getLineaRif().getIntervallo());
+				if(f.getIdFermata() == f1.getIdFermata() && f.getLineaRif() != f1.getLineaRif()){
+					if(!graph.containsEdge(f, f1)){
+						graph.addEdge(f, f1);
+						graph.addEdge(f1, f);
+						graph.setEdgeWeight(graph.getEdge(f, f1), f1.getLineaRif().getIntervallo()/60);
+						graph.setEdgeWeight(graph.getEdge(f1, f), f.getLineaRif().getIntervallo()/60);
+					}
+					
 				}
 				//System.out.println(graph.edgeSet());
 			}
@@ -66,8 +70,12 @@ public class MetroDeParisModel {
 				//Collego le stazioni vicine solo se sono sulla stessa linea. I collegamenti tra le linee sono già fatti
 				if(near.contains(f1.toString())){
 					if(f.getLineaRif().getIdLinea() == f1.getLineaRif().getIdLinea()){
-						graph.addEdge(f, f1);
-						graph.setEdgeWeight(graph.getEdge(f, f1), calcolaTempo(f, f1));
+						if(!graph.containsEdge(f, f1)){
+							graph.addEdge(f, f1);
+							graph.addEdge(f1, f);
+							graph.setEdgeWeight(graph.getEdge(f, f1), calcolaTempo(f, f1));
+							graph.setEdgeWeight(graph.getEdge(f1, f), calcolaTempo(f1, f));
+						}						
 					}
 				}
 			}
@@ -80,8 +88,8 @@ public class MetroDeParisModel {
 		double d = LatLngTool.distance(new LatLng(f1.getCoordX(), f1.getCoordY()), new LatLng(f2.getCoordX(), f2.getCoordY()), LengthUnit.KILOMETER);
 		//La velocità è specificata nella tabella linea
 		double v = ldao.getVelocity(f1, f2);
-		if(d == 0 || v == 0){
-			return 0;
+		if(d == 0 || v == 0){ //Sono la stessa fermata, aggiungo intervallo
+			return f1.getLineaRif().getIntervallo()/60;
 		}
 		return d/v;
 	}
@@ -97,9 +105,12 @@ public class MetroDeParisModel {
 				stop = f;
 			}
 		}
-		DijkstraShortestPath<Fermata, DefaultWeightedEdge> dijkstra = new DijkstraShortestPath<Fermata, DefaultWeightedEdge>(graph, start, stop);
-		GraphPath<Fermata, DefaultWeightedEdge> path = dijkstra.getPath();
-		return Graphs.getPathVertexList(path);
+		List<DefaultWeightedEdge> path = DijkstraShortestPath.findPathBetween(graph, start, stop);
+		List<Fermata> temp = new ArrayList<Fermata>();
+		for(DefaultWeightedEdge e : path){
+			temp.add(graph.getEdgeTarget(e));
+		}
+		return temp;
 	}
 	
 	public String tempoPercorso(List<Fermata> l){
